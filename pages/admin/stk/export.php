8466fb5296cd978b344a3726c4f7cc0933318176
Istighfariza Aprini <91015387+istighfarizaaprini@users.noreply.github.com>
2022-07-18 11:35:56 +0700

    up

--- pages/admin/stk/export.php
+++ pages/admin/stk/export.php
@@ -4,7 +4,7 @@ include '../jumlah_data.php';
 ?>
 <html>
 <head>
-  <title>Eksport Buku Surat Masuk | E-Book Disposisi</title>
+  <title>Buku Surat Masuk | E-Book Disposisi</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -22,34 +22,34 @@ include '../jumlah_data.php';
 				<div class="data-tables datatable-dark">
 					
                 <table class="table table-striped table-bordered table-hover" id="mauexport">
-                                            <thead class="font-weight-bold">
-                                                <tr>
-                                                    <th>No Urut</th>
-                                                    <th>No Surat</th>
-                                                    <th>Asal Surat</th>
-                                                    <th>Tanggal Diterima</th>
-                                                    <th>Tanggal Surat</th>
-                                                    <th>Keterangan</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php
-                                                $no = 1;
-                                                $hasil = mysqli_query($koneksi, "SELECT*FROM tb_stk");
-                                                while ($data = mysqli_fetch_array($hasil)) {
-                                                ?>
-                                                    <tr>
-                                                        <td><?= $no; ?></td>
-                                                        <td><?= $data['jenis']; ?></td>
-                                                        <td><?= $data['kategori']; ?></td>
-                                                        <td><?= $data['no_dokumen']; ?></td>
-                                                        <td><?= $data['judul']; ?></td>
-                                                        <td><?= $data['keterangan']; ?></td>
-                                                    </tr>
-                                                <?php $no++;
-                                                } ?>
-                                            </tbody>
-                                        </table>
+                    <thead class="font-weight-bold">
+                        <tr>
+                            <th>No Urut</th>
+                            <th>No Surat</th>
+                            <th>Asal Surat</th>
+                            <th>Tanggal Diterima</th>
+                            <th>Tanggal Surat</th>
+                            <th>Keterangan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        $hasil = mysqli_query($koneksi, "SELECT*FROM tb_stk");
+                        while ($data = mysqli_fetch_array($hasil)) {
+                        ?>
+                            <tr>
+                                <td><?= $no; ?></td>
+                                <td><?= $data['jenis']; ?></td>
+                                <td><?= $data['kategori']; ?></td>
+                                <td><?= $data['no_dokumen']; ?></td>
+                                <td><?= $data['judul']; ?></td>
+                                <td><?= $data['keterangan']; ?></td>
+                            </tr>
+                        <?php $no++;
+                            } ?>
+                    </tbody>
+                </table>
 					
 				</div>
 </div>
